933e918608f38335532e077e9c2aa47af4e3eb72
Fahiye Muhammad <fahiye254@gmail.com>
2025-05-13 19:52:31 +0300

    writing a python I/O  code with the modes r , w and a  (read ,write and append)

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,26 @@
+from pathlib import Path
+
 def write_file(file_name, file_content):
-    pass
+    file_path = Path(file_name)
+    if file_path.suffix != ".txt":
+        file_path = file_path.with_suffix(".txt")
+
+    with file_path.open("w") as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    file_path = Path(file_name)
+    if file_path.suffix != ".txt":
+        file_path = file_path.with_suffix(".txt")
+
+    with file_path.open("a") as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    file_path = Path(file_name)
+    if file_path.suffix != ".txt":
+        file_path = file_path.with_suffix(".txt")
+
+    with file_path.open("r") as file:
+        return file.read()
+
